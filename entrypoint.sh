455452b5d033f2e4d78722e7db3e75048f39c302
Stevie Caldwell <scaldwell@gasbuddy.com>
2018-11-04 22:18:56 -0500

    Remove multibinder and dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,9 @@ fi
 
 /sbin/syslogd -O /dev/stdout
 
-set +eo pipefail
+set -eo pipefail
 
-echo "[gasbuddy/haproxy] booting container. ETCD: $ETCD_NODE"
+echo "[gasbuddy/haproxy-confd] booting container. ETCD: $ETCD_NODE"
 
 function config_fail()
 {
@@ -18,20 +18,17 @@ function config_fail()
 	exit -1
 }
 
-echo "[gasbuddy/haproxy] Running multibinder deamon"
-/usr/bin/multibinder "$MULTIBINDER_SOCK" &
 
 # Loop until confd has updated the haproxy config
 n=0
 until confd -onetime -node "$ETCD_NODE"; do
-  if [ "$n" -eq "4" ];  then config_fail; fi
-  echo "[gasbuddy/haproxy] waiting for confd to refresh haproxy.cfg"
+  if [ "$n" -eq "10" ];  then config_fail; fi
+  echo "[gasbuddy/haproxy-confd] waiting for confd to refresh haproxy.cfg"
   n=$((n+1))
   sleep $n
 done
 
-echo "[gasbuddy/haproxy] Initial HAProxy config created. Starting multibinder-haproxy and confd"
-/usr/bin/multibinder-haproxy-wrapper haproxy -Ds -f /usr/local/etc/haproxy/haproxy.cfg.erb -p $HAPROXY_PID &
-echo $! > $MULTIBINDER_PID
+echo "[gasbuddy/haproxy-confd] Initial HAProxy config created. Starting haproxy and confd"
+/usr/local/sbin/haproxy -f /usr/local/etc/haproxy/haproxy.cfg -D -p /var/run/haproxy.pid 
 
 exec confd -watch=true -node "$ETCD_NODE"
