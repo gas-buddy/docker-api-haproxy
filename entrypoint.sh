beeeb2f336e5626fe8adfcded41cfb02a0a1e4de
Eric Herot <400447+eherot@users.noreply.github.com>
2020-07-09 19:48:18 -0400

    Change startup sequence and reload behavior
    Instead of starting confd and using haproxy as the main process, then attempting to use the haproxy -sf flag in the confd reload_cmd to kill the old haproxy process, we now start haproxy in the background (with `&`) and use confd as the main process (with kill -s HUP $HAPROXY_PID as the reload_cmd)

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,10 +12,20 @@ echo '[gasbuddy/api-haproxy] Booting container.'
 echo "[gasbuddy/api-haproxy] Fetching config from ETCD: $ETCD_NODE"
 
 confd -onetime -sync-only -node "$ETCD_NODE"
-exec confd -watch=true -node "$ETCD_NODE" &
 
 echo '[gasbuddy/api-haproxy] Initial HAProxy config created. Starting haproxy...'
 
-echo "[gasbuddy/api-haproxy] Initial HAProxy config created. Starting haproxy and confd"
-/usr/local/sbin/haproxy -f /usr/local/etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid
+# Using & instead of -D so that logs go to stdout
+/usr/local/sbin/haproxy \
+  -f /usr/local/etc/haproxy/haproxy.cfg \
+  -W \
+  -p $HAPROXY_PID &
+
+echo "[gasbuddy/api-haproxy] Waiting for haproxy to come online..."
+
+while [[ ! -f $HAPROXY_PID ]]; do sleep 1; done
+
 echo "[gasbuddy/api-haproxy] Started haproxy (PID: $(cat $HAPROXY_PID))"
+echo '[gasbuddy/api-haproxy] Starting confd...'
+
+confd -watch=true -node "$ETCD_NODE"
