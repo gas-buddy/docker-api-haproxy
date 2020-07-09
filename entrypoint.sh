e20a4c3ef77c10396a5b03b51ba4308cc6f0f43b
Eric Herot <400447+eherot@users.noreply.github.com>
2020-07-09 19:38:55 -0400

    Add a bunch more log output during startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,8 @@ fi
 
 set -eo pipefail
 
-echo "[gasbuddy/api-haproxy] booting container. ETCD: $ETCD_NODE"
+echo '[gasbuddy/api-haproxy] Booting container.'
+echo "[gasbuddy/api-haproxy] Fetching config from ETCD: $ETCD_NODE"
 
 function config_fail()
 {
@@ -19,5 +20,8 @@ function config_fail()
 confd -onetime -node "$ETCD_NODE"
 exec confd -watch=true -node "$ETCD_NODE" &
 
+echo '[gasbuddy/api-haproxy] Initial HAProxy config created. Starting haproxy...'
+
 echo "[gasbuddy/api-haproxy] Initial HAProxy config created. Starting haproxy and confd"
 /usr/local/sbin/haproxy -f /usr/local/etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid
+echo "[gasbuddy/api-haproxy] Started haproxy (PID: $(cat $HAPROXY_PID))"
